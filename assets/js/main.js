c6b9bd57f621249779be9f661b01ceaf6cc319b6
lucaspr <Lucaswebdeveloper@proton.me>
2023-10-09 21:54:11 -0300

    Scrollbar, scrollup and footer almost done

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,9 +31,30 @@ const blurHeader = () => {
 window.addEventListener('scroll', blurHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with scrollup class
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+/*
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 3000,
+    delay: 400,
+    // reset: true // Animations repeat
+})
+
+sr.reveal(`.home__data, .explore__data, .explore__user, .footer__container`)
+sr.reveal(`.home__card`, {delay: 600, distance: '100px', interval: 100})
+sr.reveal(`.about__data, .join__image`, {origin: 'right'})
+sr.reveal(`.about__image, .join__data`, {origin: 'left'})
+sr.reveal(`.popular__card`, {interval: 200})
+*/
